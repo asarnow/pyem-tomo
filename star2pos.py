0d88b51b506c3230244186785ba5e0a97bf59d4a
Daniel Asarnow <asarnow@msg.ucsf.edu>
2025-02-07 14:01:27 -0800

    Fix parens

--- star2pos.py
+++ star2pos.py
@@ -8,7 +8,7 @@ import sys
 
 @click.command()
 @click.argument('starpath', type=click.Path(exists=True), path_type=pathlib.Path)
-@click.argument('out', type=click.Path(), path_type=pathlib.Path)
+@click.argument('out', type=click.Path(path_type=pathlib.Path))
 @click.option('--write-mod', '-m', type=bool, default=False)
 @click.option('--sphere', '-sp', type=int, default=6)
 @click.option('--circle', '-ci', type=int, default=3)
