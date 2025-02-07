1b3c9f438a6bcef648b00ace785d1acfac88257f
Daniel Asarnow <asarnow@msg.ucsf.edu>
2025-02-07 13:23:36 -0800

    Added star2pos.py to convert particle star to IMOD text or model

---
+++ star2pos.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import click
+import starfile
+import subprocess
+import sys
+
+
+@click.command()
+@click.argument('starpath', type=click.Path(exists=True))
+@click.argument('out', type=click.Path)
+@click.argument('--write-mod', '-m', type=bool)
+@click.argument('--sphere', '-sp', type=int, default=6)
+@click.argument('--circle', '-ci', type=int, default=3)
+def main(starpath, out, write_mod, sphere, circle):
+    star_tables = starfile.read(starpath, always_dict=True)
+    df = star_tables['particles']
+    for k in ['rlnTomoName', 'rlnMicrographName']:
+        if k in df.columns:
+            break
+    gb = df.groupby(k)
+    for n, g in gb:
+        fn = out / f'{n}.pos'
+        g.to_csv(fn, index=False, header=False, sep='\t')
+        if write_mod:
+            subprocess.run(f'point2model -sc -sp {sphere} -ci {circle} {fn} {fn[:-4] + ".mod"}', shell=True)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
